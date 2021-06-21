ed79792e9c50058984469bff6c26d0f6d7c568fb
woodylucas <wlucas0311@gmail.com>
2021-06-21 16:42:25 -0400

    feat: implemented a widge section, downgraded react file to 16, installed react-twitter-embed

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "../Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's Happening</h2>
+        <TwitterTweetEmbed tweetId={"1385418510122295297"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="clucas0311"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://www.facebook.com/Clucas0311/"}
+          options={{ text: "#reactjs is awesome" }}
+        />
+      </div>
     </div>
   );
 }
